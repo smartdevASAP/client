b40209f3fbb38c6120e63b4031d964f436b01db3
kelvinTech <kelvinnjuguna357@gmail.com>
2025-11-13 10:56:02 +0300

    axios changes

--- chatApp/src/api/axios.tsx
+++ chatApp/src/api/axios.tsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000", //backend base url
+  baseURL: "https://chatapp-server-1-i5ik.onrender.com", //backend base url
+
   withCredentials: true, //to allow cookies and JWT
 });
 
